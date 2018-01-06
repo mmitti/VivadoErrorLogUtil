e74cd37b80d9f314b03f3df8f52949c3219f903b
mmitti <dev@mail.mmitti.info>
2018-01-06 18:09:01 +0900

    プログラム作成 取りあえず動く

---
+++ VivadoErrorLogUtil/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace VivadoErrorLogUtil
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Win32.WindowFinder WindowFinder;
+        String ProjectRootDir;
+        String ProjectName;
+        String CurrentLogFile;
+        Dictionary<String, DateTime> LogFiles;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LogI("START");
+            LogI("(c)mmitti 2018");
+            LogI("vivado error log tool");
+            LogI("///////////////////////");
+            WindowFinder = new Win32.WindowFinder();
+            WindowFinder.OnVivadoFound += OnVivadoFound;
+            WindowFinder.RequestSearch();
+            LogFiles = new Dictionary<string, DateTime>();
+            Task.Run(() => {
+                MainLoop();
+            });
+        }
+
+        private async void MainLoop()
+        {
+            while (true)
+            {
+                await Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                });
+                await MainTask();
+            }
+        }
+
+        private async Task MainTask()
+        {
+            var sim_dir = Path.Combine(ProjectRootDir, ProjectName + ".sim");
+            var dirs = Directory.GetDirectories(sim_dir);
+            foreach(var dir in dirs)
+            {
+                foreach(var log_name in new string[]{"elaborate", "xvlog" })
+                {
+                    var file_path = Path.Combine(dir, "behav", "xsim", log_name + ".log");
+                    if (File.Exists(file_path) && !LogFiles.ContainsKey(file_path)){
+                        LogFiles[file_path] = DateTime.MinValue;
+                        LogI(file_path + " found");
+                    }
+                }
+            }
+
+            string[] logs = null;
+            
+
+            foreach (var key in LogFiles.Keys.ToList())
+            {
+                var ts = File.GetLastWriteTime(key);
+                if (LogFiles[key] < ts)
+                {
+                    LogFiles[key] = ts;
+
+                    if (CurrentLogFile == key)
+                    {
+                        await Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            MainLog.Inlines.Clear();
+                            FileName.Inlines.Clear();
+                            FileName.Inlines.Add("No Error Log");
+                        }));
+                        CurrentLogFile = null;
+                        LogI("clear");
+                    }
+
+                    string[] tmp;
+                    try
+                    {
+                        using (var f = File.OpenRead(key))
+                        {
+                            using (var s = new StreamReader(f))
+                            {
+                                var lines = await s.ReadToEndAsync();
+                                tmp = lines.Split(new char[] { '\n', '\r' });
+                            }
+                        }
+                    }catch(Exception e)
+                    {
+                        continue;
+                    }
+                       
+                    foreach(var l in tmp)
+                    {
+                        if (l.Contains("ERROR"))
+                        {
+                            CurrentLogFile = key;
+                            logs = tmp;
+                            LogI("load:" + key);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (logs != null)
+            {
+                await Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    FileName.Inlines.Clear();
+                    
+                    var m = Regex.Match(CurrentLogFile.Replace('\\', '/').ToString(), @"^(?<root>.*\.sim/)(?<sim_dir>[^/]*)(?<xsim>/behav/xsim/)(?<logname>[^\.]*).log$");
+                    if (m.Success)
+                    {
+                        FileName.Inlines.Add(new Run(m.Groups["sim_dir"].Value) { Foreground = Brushes.Blue });
+                        FileName.Inlines.Add("/");
+                        FileName.Inlines.Add(new Run(m.Groups["logname"].Value) { Foreground = Brushes.Blue });
+                        FileName.Inlines.Add(new Run("@" + LogFiles[CurrentLogFile].ToString()) { FontSize=14});
+                    }
+
+                    MainLog.Inlines.Clear();
+                    foreach(var l in logs)
+                    {
+                        if (l.Contains("ERROR"))
+                        {
+                            MainLog.Inlines.Add(new Run(l) { Foreground = Brushes.Red });
+                        }else if (l.Contains("WARNING"))
+                        {
+                            MainLog.Inlines.Add(new Run(l) { Foreground = Brushes.Orange });
+                        }
+                        else
+                        {
+                            MainLog.Inlines.Add(new Run(l));
+                        }
+                        MainLog.Inlines.Add(new LineBreak());
+                    }
+                    MainLogScroll.ScrollToEnd();
+                    Activate();
+                }));
+            }
+        }
+
+        private void LogI(string s)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                SystemLog.Inlines.Add(new Run(s) { Foreground = Brushes.Blue });
+                SystemLog.Inlines.Add(new LineBreak());
+                SystemLogScroll.ScrollToEnd();
+            }));
+        }
+        private void LogE(string s)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                SystemLog.Inlines.Add(new Run(s) { Foreground = Brushes.Red });
+                SystemLog.Inlines.Add(new LineBreak());
+                SystemLogScroll.ScrollToEnd();
+            }));
+        }
+
+        private void OnVivadoFound(string project_name, string project_path)
+        {
+            LogI("Vivado Found:" + project_name);
+            ProjectPath.Content = project_path;
+            ProjectRootDir = Path.GetDirectoryName(project_path);
+            ProjectName = project_name;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowFinder.RequestSearch();
+        }
+    }
+}
---
+++ VivadoErrorLogUtil/WindowFinder.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VivadoErrorLogUtil
+{
+    namespace Win32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left, Top, Right, Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        
+        public int X
+        {
+            get { return Left; }
+            set { Right -= (Left - value); Left = value; }
+        }
+
+        public int Y
+        {
+            get { return Top; }
+            set { Bottom -= (Top - value); Top = value; }
+        }
+
+        public int Height
+        {
+            get { return Bottom - Top; }
+            set { Bottom = value + Top; }
+        }
+
+        public int Width
+        {
+            get { return Right - Left; }
+            set { Right = value + Left; }
+        }
+
+ 
+
+       
+        public static bool operator ==(RECT r1, RECT r2)
+        {
+            return r1.Equals(r2);
+        }
+
+        public static bool operator !=(RECT r1, RECT r2)
+        {
+            return !r1.Equals(r2);
+        }
+
+        public bool Equals(RECT r)
+        {
+            return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RECT)
+                return Equals((RECT)obj);
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+        }
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct WINDOWINFO
+    {
+        public uint cbSize;
+        public RECT rcWindow;
+        public RECT rcClient;
+        public uint dwStyle;
+        public uint dwExStyle;
+        public uint dwWindowStatus;
+        public uint cxWindowBorders;
+        public uint cyWindowBorders;
+        public ushort atomWindowType;
+        public ushort wCreatorVersion;
+
+        public WINDOWINFO(Boolean? filler) : this()   // Allows automatic initialization of "cbSize" with "new WINDOWINFO(null/true/false)".
+        {
+            cbSize = (UInt32)(Marshal.SizeOf(typeof(WINDOWINFO)));
+        }
+
+    }
+        class WindowFinder
+        {
+            private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+            public delegate void VivadoFound(String project_name, String project_path);
+            public event VivadoFound OnVivadoFound;
+            const uint WS_VISIBLE = 0x10000000;
+
+            /// <summary>
+            ///     Enumerates all top-level windows on the screen by passing the handle to each window, in turn, to an
+            ///     application-defined callback function. <see cref="EnumWindows" /> continues until the last top-level window is
+            ///     enumerated or the callback function returns FALSE.
+            ///     <para>
+            ///         Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633497%28v=vs.85%29.aspx for more
+            ///         information
+            ///     </para>
+            /// </summary>
+            /// <param name="lpEnumFunc">
+            ///     C++ ( lpEnumFunc [in]. Type: WNDENUMPROC )<br />A pointer to an application-defined callback
+            ///     function. For more information, see
+            ///     <see cref="!:https://msdn.microsoft.com/en-us/library/windows/desktop/ms633498%28v=vs.85%29.aspx">EnumWindowsProc</see>
+            ///     .
+            /// </param>
+            /// <param name="lParam">
+            ///     C++ ( lParam [in]. Type: LPARAM )<br />An application-defined value to be passed to the callback
+            ///     function.
+            /// </param>
+            /// <returns>
+            ///     <c>true</c> if the return value is nonzero., <c>false</c> otherwise. If the function fails, the return value
+            ///     is zero.<br />To get extended error information, call GetLastError.<br />If <see cref="EnumWindowsProc" /> returns
+            ///     zero, the return value is also zero. In this case, the callback function should call SetLastError to obtain a
+            ///     meaningful error code to be returned to the caller of <see cref="EnumWindows" />.
+            /// </returns>
+            /// <remarks>
+            ///     The <see cref="EnumWindows" /> function does not enumerate child windows, with the exception of a few
+            ///     top-level windows owned by the system that have the WS_CHILD style.
+            ///     <para />
+            ///     This function is more reliable than calling the
+            ///     <see cref="!:https://msdn.microsoft.com/en-us/library/windows/desktop/ms633515%28v=vs.85%29.aspx">GetWindow</see>
+            ///     function in a loop. An application that calls the GetWindow function to perform this task risks being caught in an
+            ///     infinite loop or referencing a handle to a window that has been destroyed.<br />Note For Windows 8 and later,
+            ///     EnumWindows enumerates only top-level windows of desktop apps.
+            /// </remarks>
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+            /// <summary>
+            ///     Copies the text of the specified window's title bar (if it has one) into a buffer. If the specified window is a
+            ///     control, the text of the control is copied. However, GetWindowText cannot retrieve the text of a control in another
+            ///     application.
+            ///     <para>
+            ///         Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633520%28v=vs.85%29.aspx  for more
+            ///         information
+            ///     </para>
+            /// </summary>
+            /// <param name="hWnd">
+            ///     C++ ( hWnd [in]. Type: HWND )<br />A <see cref="IntPtr" /> handle to the window or control containing the text.
+            /// </param>
+            /// <param name="lpString">
+            ///     C++ ( lpString [out]. Type: LPTSTR )<br />The <see cref="StringBuilder" /> buffer that will receive the text. If
+            ///     the string is as long or longer than the buffer, the string is truncated and terminated with a null character.
+            /// </param>
+            /// <param name="nMaxCount">
+            ///     C++ ( nMaxCount [in]. Type: int )<br /> Should be equivalent to
+            ///     <see cref="StringBuilder.Length" /> after call returns. The <see cref="int" /> maximum number of characters to copy
+            ///     to the buffer, including the null character. If the text exceeds this limit, it is truncated.
+            /// </param>
+            /// <returns>
+            ///     If the function succeeds, the return value is the length, in characters, of the copied string, not including
+            ///     the terminating null character. If the window has no title bar or text, if the title bar is empty, or if the window
+            ///     or control handle is invalid, the return value is zero. To get extended error information, call GetLastError.<br />
+            ///     This function cannot retrieve the text of an edit control in another application.
+            /// </returns>
+            /// <remarks>
+            ///     If the target window is owned by the current process, GetWindowText causes a WM_GETTEXT message to be sent to the
+            ///     specified window or control. If the target window is owned by another process and has a caption, GetWindowText
+            ///     retrieves the window caption text. If the window does not have a caption, the return value is a null string. This
+            ///     behavior is by design. It allows applications to call GetWindowText without becoming unresponsive if the process
+            ///     that owns the target window is not responding. However, if the target window is not responding and it belongs to
+            ///     the calling application, GetWindowText will cause the calling application to become unresponsive. To retrieve the
+            ///     text of a control in another process, send a WM_GETTEXT message directly instead of calling GetWindowText.<br />For
+            ///     an example go to
+            ///     <see cref="!:https://msdn.microsoft.com/en-us/library/windows/desktop/ms644928%28v=vs.85%29.aspx#sending">
+            ///         Sending a
+            ///         Message.
+            ///     </see>
+            /// </remarks>
+            [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+            static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+            /// <summary>
+            ///     Retrieves the length, in characters, of the specified window's title bar text (if the window has a title bar). If
+            ///     the specified window is a control, the function retrieves the length of the text within the control. However,
+            ///     GetWindowTextLength cannot retrieve the length of the text of an edit control in another application.
+            ///     <para>
+            ///         Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633521%28v=vs.85%29.aspx for more
+            ///         information
+            ///     </para>
+            /// </summary>
+            /// <param name="hWnd">C++ ( hWnd [in]. Type: HWND )<br />A <see cref="IntPtr" /> handle to the window or control.</param>
+            /// <returns>
+            ///     If the function succeeds, the return value is the length, in characters, of the text. Under certain
+            ///     conditions, this value may actually be greater than the length of the text.<br />For more information, see the
+            ///     following Remarks section. If the window has no text, the return value is zero.To get extended error information,
+            ///     call GetLastError.
+            /// </returns>
+            /// <remarks>
+            ///     If the target window is owned by the current process, <see cref="GetWindowTextLength" /> causes a
+            ///     WM_GETTEXTLENGTH message to be sent to the specified window or control.<br />Under certain conditions, the
+            ///     <see cref="GetWindowTextLength" /> function may return a value that is larger than the actual length of the
+            ///     text.This occurs with certain mixtures of ANSI and Unicode, and is due to the system allowing for the possible
+            ///     existence of double-byte character set (DBCS) characters within the text. The return value, however, will always be
+            ///     at least as large as the actual length of the text; you can thus always use it to guide buffer allocation. This
+            ///     behavior can occur when an application uses both ANSI functions and common dialogs, which use Unicode.It can also
+            ///     occur when an application uses the ANSI version of <see cref="GetWindowTextLength" /> with a window whose window
+            ///     procedure is Unicode, or the Unicode version of <see cref="GetWindowTextLength" /> with a window whose window
+            ///     procedure is ANSI.<br />For more information on ANSI and ANSI functions, see Conventions for Function Prototypes.
+            ///     <br />To obtain the exact length of the text, use the WM_GETTEXT, LB_GETTEXT, or CB_GETLBTEXT messages, or the
+            ///     GetWindowText function.
+            /// </remarks>
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+            static extern int GetWindowTextLength(IntPtr hWnd);
+            [return: MarshalAs(UnmanagedType.Bool)]
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern bool GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
+            public void RequestSearch()
+            {
+                EnumWindows(WindowFound, IntPtr.Zero);
+            }
+            private bool WindowFound(IntPtr hWnd, IntPtr lParam)
+            {
+                int textLen = GetWindowTextLength(hWnd);
+                if (0 < textLen)
+                {
+                    //ウィンドウのタイトルを取得する
+                    StringBuilder title = new StringBuilder(textLen + 1);
+                    GetWindowText(hWnd, title, title.Capacity);
+                    WINDOWINFO info = new WINDOWINFO();
+                    GetWindowInfo(hWnd, ref info);
+                    if ((info.dwStyle & WS_VISIBLE) == WS_VISIBLE)
+                    {
+                        Debug.WriteLine(title);
+                        var m = Regex.Match(title.ToString(), @"^(?<project>[^\s]*) - \[(?<path>[^\s]*.xpr)\] - Vivado.*$");
+                        
+                        if (OnVivadoFound != null && m.Success) OnVivadoFound(
+                             m.Groups["project"].Value, m.Groups["path"].Value
+                        );
+                    }
+                    //^(?<project>[^\s]*) - \[(?<path>[^\s]*.xpr)\] - Vivado.*$
+                    //edit_xxxxx_v1_0 - [D:/mmitti/Git/xxxx/edit_xxxxxxx_v1_0.xpr] - Vivado 2017.4
+                }
+                return true;
+            }
+        }
+    }
+}
